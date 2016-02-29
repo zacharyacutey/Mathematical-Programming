af8b0382f99e6b8de878dea2952e46047d8ffa20
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-02-29 15:06:08 -0500

    Update FiniteObject.py

--- Lib/FiniteObject.py
+++ Lib/FiniteObject.py
@@ -1,1 +1,47 @@
-class Finite
+from NumberObject import Number
+from __future__ import division
+from sympy import ceiling,floor,re,im,Abs
+#from InfiniteObject import Infinite
+sortNumber=lambda n : [Number(j) for j in sorted([i.val for i in n])]
+# class Infinite:
+#   pass
+class Finite:
+  def __init__(self,*args):
+    for i in args:
+      if i.Type=="Infinite" or i.Type=="Function":
+        raise TypeError
+    self.val=args
+    i=0
+    while i<len(self.val):
+      j=i+1
+      while j<len(self.val):
+        if self.val[i].val==self.val[j].val:
+          self.val.pop(i)
+          i-=1
+        j+=1
+      i+=1
+    self.val=sortNumber(self.val)
+    """
+    TODO Fix the sorting for a finite set if the set has a finite set
+    """
+    
+  @property
+  def Type(self):
+    return "Finite"
+  def Negative(self):
+    raise TypeError
+  def Not(self):
+    raise TypeError
+  def Positive(self):
+    return Number(0)
+  def Unpack(self):
+    if len(self.val)==1:
+      return self.val[0]
+    return self.Positive()
+  def Complement(self):
+    return Infinite(lambda n : n.IsMemberOf(self).Not())
+  def RealPart(self):
+    raise TypeError
+  def ImaginaryPart(self):
+    raise TypeError
+  def Length(self)
