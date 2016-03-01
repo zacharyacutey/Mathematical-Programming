5622c98a474d9451ac5da19bef869c6b77796978
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-03-01 11:33:17 -0500

    Update InfiniteObject.py

--- Lib/InfiniteObject.py
+++ Lib/InfiniteObject.py
@@ -2,3 +2,13 @@ from NumberObject import Number
 from FiniteObject import Finite
 from __future__ import division
 from sympy import re,im,Abs,floor,ceiling
+class Infinite:
+  #Infinite paramteter takes local vars and returns a tuple consisting of (value,condition)
+  def __init__(self,val):
+    self.val=val
+  @property Type(self):
+    return "Infinite"
+  def Negative(self):
+    raise TypeError
+  def Not(self):
+    raise TypeError
