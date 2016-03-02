ab04a47c0667cdf10a42e19cad23068abbc96e19
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-03-02 17:06:12 -0500

    Update VariableObject.py

--- Lib/VariableObject.py
+++ Lib/VariableObject.py
@@ -1,2 +1,18 @@
 from __future__ import division
-from sympy import re,im,Abs,floor,ceiling,
+from sympy import re,im,Abs,floor,ceiling
+class Variable:
+  def __init__(self,val):
+    self.val=val
+  @property
+  def Type(self):
+    return self.val.Type
+  def Positive(self):
+    return self.val.Positive()
+  def Negative(self):
+    return self.val.Negative()
+  def Complement(self):
+    return self.val.Complement()
+  def Not(self):
+    return self.val.Not()
+  def Unpack(self):
+    return self.val.Unpack()
