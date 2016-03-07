38a0a18ccda4fc489ac3f394fe2c4bea72903fdb
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-03-07 07:02:53 -0500

    Update FunctionObject.py

--- Lib/FunctionObject.py
+++ Lib/FunctionObject.py
@@ -1,3 +1,47 @@
 from __future__ import division
 from sympy import floor,ceiling,Abs,re,im
 from NumberObject import Number
+class Function:
+  def __init__(self,val):
+    self.val=val
+  @property
+  def Type(self):
+    return "Function"
+  def Negative(self):
+    raise TypeError
+  def Not(self):
+    raise TypeError
+  def Positive(self):
+    return Function(self.val)
+  def Unpack(self):
+    return self.Positive()
+  def Complement(self):
+    raise TypeError
+  def RealPart(self):
+    raise TypeError
+  def ImaginaryPart(self):
+    raise TypeError
+  def Length(self):
+    raise TypeError
+  def Floor(self):
+    raise TypeError
+  def Ceiling(self):
+    raise TypeError
+  def AbsoluteValue(self):
+    raise TypeError
+  def Add(self,arg):
+    raise TypeError
+  def Minus(self,arg):
+    raise TypeError
+  def Multiply(self,arg):
+    raise TypeError
+  def Divide(self,arg):
+    raise TypeError
+  def Modulo(self,arg):
+    raise TypeError
+  def Power(self,arg):
+    raise TypeError
+  def HasMember(self,arg):
+    raise TypeError
+  def IsMemberOf(self,arg):
+    raise TypeError
