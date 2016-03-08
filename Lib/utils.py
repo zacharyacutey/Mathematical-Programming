baab68540f5673aaab9b7c6462ea643b5b7db5ef
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-03-07 19:06:01 -0500

    Update utils.py

--- Lib/utils.py
+++ Lib/utils.py
@@ -1,4 +1,5 @@
 atd={"$img":"ℑ"}
+#Just some random utilities that don't fit in to the (soon to be) massive compiler/tokenizer/transpiler/interpreter/parser page
 #To Do:
 #ALLOW WINDOWS FUNCTIONALITY!!!!!!!!!!!!!!!!
 def ascii_to_unicode(s):
@@ -29,11 +30,26 @@ def remove_w(s):
       i+=1
   return r
 def alphabetic(c):
-  return c in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  return c in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" #To put the special chars in the is_unary code
 def numeric(c):
   return c in "0123456789"
 def alphanumeric(c):
   return alphabetic(c) or numeric(c)
-def next_sep(s_,p):
-  s=s_[p:]
-  #Resume here
+def is_unary(s,p):
+  if p==0:
+    return True
+  return not alphanumeric(s[p-1])
+def unary_char(c):
+  if c=="+":
+    return "∔"
+  if c=="-":
+    return "∸"
+  return c
+def replace_unary(s):
+  r=[]
+  for i in range(len(s)):
+    if is_unary(s,i):
+      r.append(unary_char(s[i]))
+    else:
+      r.append(c)
+  return ''.join r
