00a6fed3819300f1b2a2cf5d36b7dfa1d9ed242d
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-03-19 15:15:27 -0400

    Update test.py

--- Lib/test.py
+++ Lib/test.py
@@ -3,3 +3,4 @@ from FiniteObject import Finite
 from InfiniteObject import Infinite
 from FunctionObject import Function
 from sympy import S
+#A file that imports everything needed for testing the python Library files
