e72e7613ebd705ee9ebcc447cf65651191ea1603
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-03-19 12:24:44 -0400

    Update test.py

--- Lib/test.py
+++ Lib/test.py
@@ -2,6 +2,4 @@ from NumberObject import Number
 from FiniteObject import Finite
 from InfiniteObject import Infinite
 from FunctionObject import Function
-
-print Number(1).Add(Number(2))
-print Number(4).Minus(Number(3))
+from sympy import S
