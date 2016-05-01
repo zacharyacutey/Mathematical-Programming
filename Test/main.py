1a8484e3c95c157a13875e3d07aca43034f8e159
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-05-01 19:09:03 -0400

    Update main.py

--- Test/main.py
+++ Test/main.py
@@ -2,3 +2,6 @@ def is_NonZero(arg):
   return arg in [['1'],['2'],['3'],['4'],['5'],['6'],['7'],['8'],['9']]
 def is_Digit(arg):
   return is_NonZero(arg) or arg==['0']
+def is_Number(arg):
+  from functools import reduce
+  return is_NonZero(arg) and reduce(lambda x,y:x and y,map(lambda x : is_Digit(x),arg))
