063d768b9c791353840c7ea75b5247d43c6909b8
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-05-01 17:39:46 -0400

    Update Parser.py

--- Lib/Parser.py
+++ Lib/Parser.py
@@ -6,3 +6,21 @@ def seperate_lines(s_):
     if i!="":
       r.append(i)
   return r
+def is_lambda_expression(arg):
+  """
+  Tells if a string is a lambda expresion, EBNF 'Function'
+  """
+  count = 0
+  for i in [j[0] for j in arg]: #Make sure for the stream of tokens
+    if count == 0:
+      if i == atd['$fun']:
+        return True
+    elif count < 0:
+      print("DAFUQ? AN ERROR DID APPEAR! I SHOULD BE THE ONLY ONE USING THIS, SO STOP MAKING TYPOS B****!")
+      exit()
+    else:
+      if i == '(':
+        count += 1
+      if i == ')':
+        count -= 1
+  return False
